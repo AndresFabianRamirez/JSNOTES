4f043864869398d81943c5fe62680d0ecbd0d543
andres <andrecitoramirez11@gmail.com>
2023-07-05 14:36:04 -0500

    palitos ejemplo

--- js_desdecero/katas.js
+++ js_desdecero/katas.js
@@ -1,35 +1,7 @@
-function snail(arrT){
-  let l =[];
- while (true){ 
-    l = l.concat(arrT[0]);
-    arrT.shift();
-    if (arrT ==0){
-      break; 
-    } 
-    let i =0;
-    let vars = arrT.length -1;
-     while(i<vars){
-      l = l.concat(arrT[i].at(-1));
-      arrT[i].pop();
-      i++;
-    }
-     l = l.concat(arrT.at(-1).reverse()); 
-    arrT.pop();
-    if (arrT ==0){
-      break; 
-    } 
-   
-    arrT.reverse();
-    for (y in arrT){
-      let f =arrT[y].at(0);
-      l = l.concat(f);
-      arrT[y].shift();
-    }
-    arrT.reverse();    
-  } 
-  return l;
-}
+function palitos(entero){
   
-
-console.log(snail([[1, 2, 3, 4, 5, 6], [20, 21, 22, 23, 24, 7], [19, 32, 33, 34, 25, 8], [18, 31, 36, 35, 26, 9], [17, 30, 29, 28, 27, 10], [16, 15, 14, 13, 12, 11]])); 
- 
+  let list = [6,8,13,18,22,27,33,36,43,49];
+    return list[Number(entero)];
+}
+console.log(palitos(0));
+list 
