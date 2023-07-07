e6755b23b226541079647a0f8aece3d3351d0970
andres <andrecitoramirez11@gmail.com>
2023-07-07 17:27:17 -0500

    ejercicio de las direcciones

--- js_desdecero/katas.js
+++ js_desdecero/katas.js
@@ -1,8 +1,17 @@
-function numerosI(str){
-  let a = str.split("");
-  console.log(a);
-  let b = a.filter(x=> parseInt(x)==Number(x)).join("");
-  console.log(b);
-}
-numerosI("dsfgasdrg545a6"); 
- 
+
+  function dirReduc(arr){
+    let arr1 = arr.map(element => element.toUpperCase());
+    for(let i =1;i<= parseInt(arr1.length /2)+1;i++){
+      for(let i =0;i<arr1.length - 1;i++){
+        if ((arr1[i]=="NORTH" && arr1[i+1]=="SOUTH")|| (arr1[i]=="SOUTH" && arr1[i+1]=="NORTH") ){
+           arr1.splice(i,2);
+        }
+        if ((arr1[i]=="EAST" && arr1[i+1]=="WEST")||(arr1[i]=="WEST" && arr1[i+1]=="EAST")){
+          arr1.splice(i,2);
+       }
+      }
+    }
+    return arr1;
+  }
+
+console.log(dirReduc(['North', 'South', 'South', 'East', 'West', 'North', 'West']));
